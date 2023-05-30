9beb04166f248fce1e00c0001d92fc4a990b1c17
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2023-05-30 18:01:27 +0200

    added all components

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,16 +1,15 @@
-import mongoose from "mongoose";
-import clientPromise from "@/lib/mongodb";
-import { Product } from "@/models/Product";
 
-export async function handler(req, res) {
+import {Product} from "@/models/Product";
+import {mongooseConnect} from "@/lib/mongoose";
+
+export default async function handle(req, res) {
     const {method} = req;
-    mongoose.Promise = clientPromise;
-    if(method === 'POST'){
-        const {title, description, price} = req.body;
+    await mongooseConnect();
+    if (method === 'POST') {
+        const {title,description,price} = req.body;
         const productDoc = await Product.create({
-            title, description, price,
+          title,description,price,
         })
         res.json(productDoc);
-
-    }
+      }
 }
