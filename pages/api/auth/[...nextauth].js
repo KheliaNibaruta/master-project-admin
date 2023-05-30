d1b7b61492c5f542b2bfa6e2278217455f622d8a
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2023-05-30 18:57:06 +0200

    📦️updated packages

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,37 @@
-import clientPromise from '@/lib/mongodb'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-import NextAuth from 'next-auth'
-//import AppleProvider from 'next-auth/providers/apple'
-//import FacebookProvider from 'next-auth/providers/facebook'
+import NextAuth, {getServerSession} from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-//import EmailProvider from 'next-auth/providers/email'
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
-export default NextAuth({
+const adminEmails = ['knibaruta@gmail.com'];
+
+export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
-    // OAuth authentication providers...
-    /*AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
-    }),*/
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-    // Passwordless / email sign in
-    /*EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: 'NextAuth.js <no-reply@example.com>'
-    }),*/
   ],
-  adapter: MongoDBAdapter(clientPromise)
-})
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const {data: session} = useSession();
-
   return(
     <Layout>
       <div className="text-blue-900 flex justify-between">
@@ -12,7 +11,7 @@ export default function Home() {
         </h2>
         <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
           <img src={session?.user?.image} className="w-6 h-6"/>
-          <span className="py-1 px-2">
+          <span className="px-2">
             {session?.user?.name}
           </span> 
         </div>
